0e25d09dd4c8f4edc380b11a5ed3529d0fc2de9e
MidouDev <htmidou@gmail.com>
2019-04-14 17:44:53 +0200

    Feat []: prettier setup

--- App.js
+++ App.js
@@ -1,46 +1,40 @@
-import React, {Component} from 'react';
-import {Platform} from 'react-native';
+import React from 'react';
 import styled from 'styled-components/native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default App = () => (
+  <Container>
+    <WelcomeContainer>
+      <WelcomeText>Billiards tournaments platform</WelcomeText>   
+    </WelcomeContainer>
+    <InstructionText>To create a new tournament choose one of the following options :</InstructionText>
+  </Container>
+);
 
-export default class App extends Component {
-  render() {
-    return (
-      <Container>
-        <WelcomeText>Welcome to xHub's billiards tournaments platform !</WelcomeText>
-        <InstructionText>To create a new tournament choose one of the following options :</InstructionText>
-        <InstructionText>{instructions}</InstructionText>
-      </Container>
-    );
-  }
-}
 
 const Container = styled.View`
   flex: 1;
+  /*justify-content: center;*/
+  align-items: center;
+  background-color: #f2cd5d;
+  padding: 10px;
+`
+const WelcomeContainer = styled.View`
+  height: 25%;
   justify-content: center;
-  //align-items: center;
-  background-color: #ffe4c4;
+  align-items: center;
 `
 
 const WelcomeText = styled.Text`
-  font-size: 20;
+  font-size: 30;
+  color: #d741a7;
   text-align: center;
-  margin-top: 10;
-  margin-bottom: 10;
-  margin-left: 10;
-  margin-right: 10;
-  width: 70%;
 `
 const InstructionText = styled.Text`
   text-align: center;
   color: #333333;
   margin-bottom: 5;
   width: 85%;
+  font-size: 15;
+  color: #3A1772;
 `
 
